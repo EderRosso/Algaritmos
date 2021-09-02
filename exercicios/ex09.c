a544a1440e78691c14a1638df60fab8bd61b719d
EderRosso <ederderosso@rede.ulbra.br>
2021-09-02 17:28:43 -0300

    começo do ex009

--- exercicios/ex09.c
+++ exercicios/ex09.c
@@ -1,1 +1,10 @@
-   
+// Faça um programa que calcule e mostre a área de um trapézio.
+// Sabe-se que: A = ((base maior + base menor) * altura)/2
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
